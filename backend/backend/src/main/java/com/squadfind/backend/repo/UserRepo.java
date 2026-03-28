58b4ed0d593e4bdd023d315577e78f4f5a4f3cb5
stxp24 <dncurrie24@gmail.com>
2026-03-28 13:43:08 -0400

    UserRepo interface constructed

--- backend/backend/src/main/java/com/squadfind/backend/repo/UserRepo.java
+++ backend/backend/src/main/java/com/squadfind/backend/repo/UserRepo.java
@@ -1,4 +1,16 @@
 package com.squadfind.backend.repo;
 
-public class UserRepo {
+import com.squadfind.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    boolean existsBySquadFindUsername(String squadFindUsername);
+    boolean existsByEmail(String email);
+    Optional<User> findBySquadFindUsername(String squadFindUsername); // Returns an Optional<User> because the user may not exist
+
 }
