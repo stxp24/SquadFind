7848be546a27e677062dc0163e129bd1dd61a6bb
stxp24 <dncurrie24@gmail.com>
2026-03-29 13:26:16 -0400

    repo layer implemented

--- backend/backend/src/main/java/com/squadfind/backend/repo/GameRepo.java
+++ backend/backend/src/main/java/com/squadfind/backend/repo/GameRepo.java
@@ -1,4 +1,11 @@
 package com.squadfind.backend.repo;
 
-public class GameRepo {
+import com.squadfind.backend.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface GameRepo extends JpaRepository<Game, Long> {
+    boolean existsByGameName(String gameName);
+    Optional<Game> findByGameName(String gameName);
 }
--- backend/backend/src/main/java/com/squadfind/backend/repo/UserGameRepo.java
+++ backend/backend/src/main/java/com/squadfind/backend/repo/UserGameRepo.java
@@ -1,4 +1,12 @@
 package com.squadfind.backend.repo;
 
-public class UserGameRepo {
+import com.squadfind.backend.model.UserGame;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserGameRepo extends JpaRepository<UserGame, Long> {
+    boolean existsByUserIdAndGameId(Long userId, Long gameId);
+    List<UserGame> findByUserId(Long userId);
+    List<UserGame> findByGameId(Long gameId);
 }
