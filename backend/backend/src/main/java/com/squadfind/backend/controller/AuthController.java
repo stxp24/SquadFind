88eaa4a6a9344543340e985d6fec703b6fb66918
stxp24 <dncurrie24@gmail.com>
2026-04-07 22:04:21 -0400

    AuthController impl.

--- backend/backend/src/main/java/com/squadfind/backend/controller/AuthController.java
+++ backend/backend/src/main/java/com/squadfind/backend/controller/AuthController.java
@@ -1,4 +1,32 @@
 package com.squadfind.backend.controller;
 
+import com.squadfind.backend.dto.LoginUserRequest;
+import com.squadfind.backend.dto.RegisterUserRequest;
+import com.squadfind.backend.model.User;
+import com.squadfind.backend.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Long> registerUser(@RequestBody RegisterUserRequest request) {
+        Long userId = userService.registerUser(request).getId();
+        return ResponseEntity.ok(userId);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> loginUser(@RequestBody LoginUserRequest request) {
+        User loggedIn = userService.login(request);
+        return ResponseEntity.ok(loggedIn);
+    }
 }
